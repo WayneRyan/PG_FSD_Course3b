c8a320d271087d05aed417be99437dd00c0758ec
Wayne J. Ryan <wryan@smithdrug.com>
2022-05-14 10:55:57 -0400

    Starting first project for this cohort of Course3

---
+++ firstProject/src/main/java/com/example/firstProject/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.firstProject;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
